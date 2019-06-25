ff38dc128c542695d5fa4508e003fc831a765be8
tflahaul <thomas.pro@tutanota.com>
2019-06-25 22:29:55 +0200

    Modifications

--- arena.h
+++ arena.h
@@ -6,13 +6,15 @@
 /*   By: thflahau <thflahau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/23 19:33:12 by thflahau          #+#    #+#             */
-/*   Updated: 2019/06/24 21:10:31 by thflahau         ###   ########.fr       */
+/*   Updated: 2019/06/25 22:07:25 by thflahau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARENA_H
 # define ARENA_H
 
+# include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
 # include <stdint.h>
 # include "../include/op.h"
@@ -33,67 +35,165 @@
 #  error "Your compiler does not use the C preprocessor, try using GCC"
 # endif /* __GNUC__ */
 
-typedef struct			s_listhead {
-	struct s_listhead	*next;
-	struct s_listhead	*prev;
-}						t_listhead;
+# define FILE_FORMAT		".cor"
 
-typedef struct			s_process {
-	t_listhead			list;
-	char				registers[REG_NUMBER][REG_SIZE]; // ?
-	uint64_t			instruction;	// Type à définir
-}						t_process;
+typedef struct				s_listhead {
+	struct s_listhead		*next;
+	struct s_listhead		*prev;
+}							t_listhead;
 
-typedef struct			s_player {
-	char				name[64];		// Players name (cannot exceed 63)
-	t_process			processes;		// Players instruction list (head)
-}						t_player;
+typedef struct				s_process {
+	t_listhead				list;
+	char					registers[REG_NUMBER][REG_SIZE]; // ?
+	uint64_t				instruction;	// Type à définir
+}							t_process;
 
 /*
-**	|16|15|14|...|8|7|...|1|
+**	@id: 		Players number
+**	@name: 		Players name
+**	@processes:	Head of players processes list
+**	@next:		Pointer to next player
+**
+**	List is singly linked as we dont expect to have a large amount of players.
+*/
+typedef struct				s_player {
+	uint32_t				id;
+	char					name[64];
+	t_process				processes;
+}							t_player;
+
+/*
+**	@options:	Bitmap used to keep track of executable's options
+**	@max_cycles: Maximum amount of cycles before ending the game
 */
 # pragma pack(push, 2)
 
-typedef struct			s_arena_state {
-	uint16_t			options;
-	uint32_t			max_cycles;
-}						t_arena_state;
+typedef struct				s_arena_state {
+	uint16_t				options;
+	uint32_t				max_cycles;
+	uint8_t					arena[MEM_SIZE];
+}							t_arena_state;
 # pragma pack(pop)
 
-static t_arena_state	g_arena;
+static t_arena_state		g_arena;
 
-# define OPTION_DUMP	0x008000u
-# define OPTION_NBR		0x004000u
-# define EXTRACT_REG(x)	(x & 0x1fu)
-# define MAX_CYCLES		(10 * 1000 * 1000)
-# define LIST_INIT(ptr)	{&(ptr), &(ptr)}
-# define EXTRACTDUMP	(g_arena.options) // mask
-# define BUFF_SIZE		1024
+# define OPTION_DUMP		0x008000u
+# define OPTION_NBR			0x004000u
+# define DUMPMASK			0x3f80u
+# define DUMP_SHIFT			0x7u
+# define BUFF_SIZE			1024
+# define EXTRACT_REG(x)		(x & 0x1fu) __deprecated
+# define EXTRACT_DUMP(x)	((x & DUMPMASK) >> DUMP_SHIFT)
+# define MAX_CYCLES			(10 * 1000 * 1000)
+# define LIST_INIT(ptr)		{&(ptr), &(ptr)}
 
-typedef char			t_instruction;
-typedef char			t_arena_memory;
+typedef char				t_instruction;
+typedef char				t_arena_memory;
 
 __attribute__((always_inline))
-static inline void		ft_arena_init_params(void)
+static inline void			ft_arena_init_params(void)
 {
 	g_arena.options = 0;
 	g_arena.max_cycles = MAX_CYCLES;
 }
 
+static void					ft_dump_memory(void)
+{
+	register uint16_t		index;
+
+	index = 0;
+	while (++index < MEM_SIZE);
+}
+
+/*
+**	Attention:
+**	- Si l'id passé en paramètre est déjà donné à un autre joueur
+**	Possible de donner d'abord des numéros aux joueurs avec id spécifié
+**	puis donner les id libres aux autres ensuite.
+*/
+static inline t_player		*ft_memalloc_player(uint32_t id)
+{
+	t_player				*node;
+
+	node = (t_player *)malloc(sizeof(*node));
+	if (LIKELY(node != NULL))
+		__builtin_memset(node, 0, sizeof(*node));
+	return (node);
+}
+
+/*
+**	--- LISTS ---
+**
+**	These are easy-to-use, highly portable functions for manipulating
+**	circular linked lists. This implementation comes from the Linux
+**	kernel source tree, file `include/linux/list.h`
+*/
 __attribute__((always_inline))
-static inline void		ft_list_init_head(t_listhead *head)
+static inline void			ft_list_init_head(t_listhead *head)
 {
 	head->next = head;
 	head->prev = head;
 }
 
-static inline int		ft_extract_data_from_instruction(void)
+/*
+**	Insert a new entry between two known consecutive entries (prev & next).
+**	This is only for internal list manipulation where we know the `prev`
+**	and `next` pointers.
+*/
+__attribute__((always_inline))
+static inline void			ft_list_add(t_listhead *node,
+										t_listhead *prev,
+										t_listhead *next)
 {
-	return (0);						// En fonction du format de l'instruction
+	next->prev = node;
+	node->next = next;
+	node->prev = prev;
+	prev->next = node;
+}
+
+/*
+**	Insert a new entry AFTER the specified head. This is good for
+**	implementing stacks.
+*/
+__attribute__((always_inline))
+static inline void			ft_list_push(t_listhead *node, t_listhead *head)
+{
+	ft_list_add(node, head, head->next);
 }
 
 __attribute__((always_inline))
-static inline void		ft_retative_indexing(int *map, uint64_t addr)
+static inline void			ft_list_pop(t_listhead *node)
+{
+	node->next->prev = node->prev;
+	node->prev->next = node->next;
+	free((void *)ft_queue_entry(node));
+}
+
+/*
+**	Insert a new entry BEFORE the specified head. This is good for
+**	implementing queues.
+*/
+__attribute__((always_inline))
+static inline void			ft_list_add_tail(t_listhead *node, t_listhead *head)
+{
+	ft_list_add(node, head->prev, head);
+}
+
+static inline int32_t		ft_valid_file_fmt(char const *file, char const *fmt)
+{
+	if (UNLIKELY(file == NULL))
+		return (EXIT_FAILURE);
+	file = file + strlen(file) - (sizeof(char) << 2);
+	return (strcmp(file, fmt) ? EXIT_FAILURE : EXIT_SUCCESS);
+}
+
+static inline int			ft_extract_data_from_instruction(void)
+{
+	return (0);				// En fonction du format de l'instruction
+}
+
+__attribute__((always_inline))
+static inline void			ft_retative_indexing(int *map, uint64_t addr)
 {
 	map[addr % IDX_MOD] = ft_extract_data_from_instruction();
 }
--- main.c
+++ main.c
@@ -6,36 +6,31 @@
 /*   By: thflahau <thflahau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/24 18:08:13 by thflahau          #+#    #+#             */
-/*   Updated: 2019/06/24 21:09:58 by thflahau         ###   ########.fr       */
+/*   Updated: 2019/06/25 21:34:08 by thflahau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "arena.h"
-#include <string.h>
-#include <stdio.h>
 
-#define DUMPMASK		0x3f80u
-#define DUMP_SHIFT		0x7u
-#define EXTRACT_DUMP(x)	((x & DUMPMASK) >> DUMP_SHIFT)
-
-// Current dump and players number values cannot exceed 127 as they are
-// written on 7 bits
-static uint16_t			ft_parse_options(char const **option, uint16_t index)
+__attribute__((deprecated)) // FONCTIONNE PAS COMME IL FAUT
+static uint16_t			ft_parse_options(char const **argv, uint16_t index)
 {
-	if (strcmp((option[index] + 1), "dump") == 0)
+	if (strcmp((argv[index] + 1), "dump") == 0)
 	{
 		g_arena.options |= OPTION_DUMP;
-		if (LIKELY(option[++index] != NULL))
-			g_arena.options |= ((atoi(option[index]) & 0x7fu) << DUMP_SHIFT);
-		printf("dump = %u\n", EXTRACT_DUMP(g_arena.options));
+		if (LIKELY(argv[++index] != NULL))
+			g_arena.max_cycles = atoi(argv[index]);
+		printf("dump = %u\n", g_arena.max_cycles);
 	}
-	else if (strcmp((option[index] + 1), "n") == 0) // unsafe
+	else if (strcmp((argv[index] + 1), "n") == 0)
 	{
 		g_arena.options |= OPTION_NBR;
-		if (LIKELY(option[++index] != NULL))
-			g_arena.options |= (atoi(option[index]) & 0x7fu);
+		if (LIKELY(argv[++index] != NULL))
+			g_arena.options |= (atoi(argv[index]) & 0x7fu);
 		printf("n = %u\n", g_arena.options & 0x7fu);
 	}
+	else
+		printf("corewar: illegal option -- %s\n", argv[index] + 1);
 	return (index);
 }
 
@@ -45,8 +40,14 @@ static void				ft_parse_players(char const **argv)
 
 	index = 0;
 	while (argv[++index] != NULL)
+	{
 		if (*argv[index] == '-')
 			index = ft_parse_options(argv, index);
+		else if (ft_valid_file_fmt(argv[index], FILE_FORMAT) == EXIT_SUCCESS)
+			printf("new player\n");
+		else
+			exit(EXIT_FAILURE); // Point de sortie
+	}
 }
 
 int						main(__unused int argc, char const **argv)
