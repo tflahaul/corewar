06403372b06e0d6dcd756635229870552009cb8b
cl4k <skart@gmx.fr>
2019-09-11 17:33:57 +0200

    modification de zorkbeater, il utilise le fork pour se s2parer , l'un boucle avec des vies et l'autre va voler le processus adverse

--- warriors/zorkbeater.s
+++ warriors/zorkbeater.s
@@ -1,110 +1,15 @@
 .name "zorkbeater"
 .comment "having a fight with zork"
 
-start:
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
-st r1, 6
-live %0
 st r1, 6
 live %0
 st r1, 6
 live %0
 
 work:
-ld %50331648, r4       #03 00 00 00
-ld %1879375874, r5     #70 05 00 02
-ld %445, r6            #jumper
-ld %1879310785, r10
-ld %1879376317, r11
-st r4, 449
-st r10, 445
-st r4, 444
-st r11, 440
-fork %:looplive
+fork %:thief
 st r1, 6
 live %0
-fork %:kroz
-ld %83887619 ,r5       #50 00 06 03
-ld %0, r16
-zjmp %221
-
-kroz:
-ld %1879375874, r5     #70 05 00 02
-ld %0, r16
-zjmp %200
 
 looplive:
 sti r1, %:live, %1
@@ -113,3 +18,9 @@ ld %0, r16
 live: 
 live %1
 zjmp%:live
+
+
+thief:
+ld %0, r16
+zjmp %510
+
